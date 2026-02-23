0c6651180c214be82adf3081f87cb9473aad456f
Adrián Domínguez <adriandomc@gmail.com>
2026-02-23 15:06:46 -0600

    feat: add stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://adriandomc.com",
+  image: {
+    service: { entrypoint: "astro/assets/services/noop" },
+  },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@use "/src/styles/_variables.scss" as *;`,
+          additionalData: `@use "sass:color"; @use "/src/styles/_variables.scss" as *;`,
         },
       },
     },
   },
+
+  integrations: [mdx(), sitemap()],
 });
